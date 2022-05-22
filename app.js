e278c00f827cc19fe0f41652b0910dffb55623a6
teniolao <Tennybetty100@gmail.com>
2022-05-22 22:19:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  let today = new Date();
+  //const currentDay = today.getDay();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
