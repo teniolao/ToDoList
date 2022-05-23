1f50a7fd1b3a38746a1841913243a74a0d238d66
teniolao <Tennybetty100@gmail.com>
2022-05-23 10:45:50 +0100

    design layout and new work path

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -17,16 +19,35 @@ app.get("/", function (req, res) {
     month: "long",
   };
 
+  let yearOption = {
+    year: "numeric",
+  };
+
+  let year = today.toLocaleDateString("en-US", yearOption);
   let currentDay = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: currentDay, newListItems: items });
+  res.render("list", {
+    listTitle: currentDay,
+    newListItems: items,
+    year: year,
+  });
 });
 
 app.post("/", (req, res) => {
+  //console.log(req.body);
   let item = req.body.newItem;
-  items.push(item);
-  console.log(item);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+  console.log("Server started on port 3000");
 });
 
 app.listen(3000, function () {
